5b9c581529e37be8a0b04a5084f5eaabca07391b
pilotto8 <66770848+pilotto8@users.noreply.github.com>
2021-07-29 22:01:58 +0200

    Prova di potenza

---
+++ Bici_smart/Bici_smart.ino
@@ -0,0 +1,17 @@
+#include "settings.h"
+void setup() {
+  pinMode(6, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i < max_power; i++){
+    analogWrite(6, i);
+    delay(10);
+  }
+  for(int i = 0; i < max_power; i++){
+    analogWrite(6, max_power - i);
+    delay(10);
+  }
+}
